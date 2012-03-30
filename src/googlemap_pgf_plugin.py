ae72088a05a76c81acbb6b65a27711474a4d4849
Anders Henja <anders@baltrad.eu>
2012-03-30 11:34:49 +0200

    Ticket 30: make google map rule a proper rule

--- src/googlemap_pgf_plugin.py
+++ src/googlemap_pgf_plugin.py
@@ -27,6 +27,34 @@ compatible png file.
 import GmapCreator
 import GmapLegend
 import os
+
+## Creates a dictionary from a rave argument list
+#@param arglist the argument list
+#@return a dictionary
+def arglist2dict(arglist):
+  result={}
+  for i in range(0, len(arglist), 2):
+    result[arglist[i]] = arglist[i+1]
+  return result
+
+##
+# Converts a string into a number, either int or float
+# @param sval the string to translate
+# @return the translated value
+# @throws ValueError if value not could be translated
+#
+def toNumber(sval):
+  if isinstance(sval, (int, long)):
+    return float(sval)
+  elif isinstance(sval, float):
+    return sval
+  else:
+    try:
+      return int(sval)
+    except ValueError, e:
+      return float(sval)
+
+  
 #
 # @param files - an array of files, only first file will be used
 # @param arguments array of two arguments, ["outfile", <name of file to be saved>]
@@ -35,18 +63,17 @@ import os
 def generate(files, arguments):
   creator = GmapCreator.GmapCreator(files[0])
   img = creator.create_image()
-  l = len(arguments)
   filename=None
+  args = arglist2dict(arguments)
   kw = {}
-  for i in range(l):
-    if arguments[i] == "outfile":
-      filename = arguments[i+1]
-    if arguments[i] == "zr_a":
-      kw["zr_a"] = float(arguments[i+1])
-    if arguments[i] == "zr_b":
-      kw["zr_b"] = float(arguments[i+1])
-                         
   
+  if "outfile" in args.keys():
+    filename = args["outfile"]
+  if "zr_a" in args.keys():
+    kw["zr_a"] = toNumber(args["zr_a"])
+  if "zr_b" in args.keys():
+    kw["zr_b"] = toNumber(args["zr_b"])
+
   dname = os.path.dirname(filename)
   if not os.path.exists(dname):
     os.makedirs(dname)
@@ -60,7 +87,9 @@ def generate(files, arguments):
                                      offset=creator._intercept,
                                      **kw).save(legend_name)
     #creator.gmappalette().legend(title="dbz", legendheight=196).save(legend_name)
+
   return None
 
 if __name__ == '__main__':
-    pass
+  pass
+
